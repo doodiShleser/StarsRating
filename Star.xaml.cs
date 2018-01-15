609db649fa728705162a61c1fb663c04fd08fd2b
doodiShleser <35187941+doodiShleser@users.noreply.github.com>
2018-01-15 23:27:20 +0200

    Update Star.xaml.cs

--- Star.xaml.cs
+++ Star.xaml.cs
@@ -18,10 +18,12 @@ namespace PLWPF
 {
     /// <summary>
     /// Interaction logic for Star.xaml
+    ///Specifically I coding for my app. The star functions in two different situations,
+   /// for client that ranks his server and for server displays his rating status
     /// </summary>
     public partial class Star : UserControl
     {
-        int idChild;
+        int idChild;//the client ID
         public Star()
         {
             InitializeComponent();
@@ -30,34 +32,33 @@ namespace PLWPF
         /// CTOR for star
         /// </summary>
         /// <param name="r">1 0r 0 for brush.color of star</param>
-        /// <param name="id">0 if is a nannyRating or idcChild if is for using by mother</param>
-        public Star(int r, int id=0)
-            //to seperate between mother with function to nanny just with presentation 
+        /// <param name="id">0 for just present status of rating, not play as button</param>
+        public Star(int r, int id=0) 
         {
             InitializeComponent();
             if (r == 1)
                 myStar.Background = Brushes.Yellow;
-            idChild = id;
             if (id == 0)
                 myStar.IsEnabled = false;
+            idChild = id;
         } 
         private void myStar_Click(object sender, RoutedEventArgs e)
         {
-            Child c = new Child();
+            Child c = new Child();//define Client
             if(idChild != 0)
-                c = BL_imp.GetInstance().getChild().Find(x=>x.ID == idChild);
+                c = //... //in my app: BL_imp.GetInstance().getChild().Find(x=>x.ID == idChild);
             if (myStar.Background==Brushes.Yellow)
             {
                 myStar.Background = Brushes.White;
-                c.stars--;
-                BL_imp.GetInstance().updateChild(c);
+                c.stars--;//he choose to decrease his rating for server
             }
             else
             {
                 myStar.Background = Brushes.Yellow;
-                c.stars++;
-                BL_imp.GetInstance().updateChild(c);
+                c.stars++;//he choose to crease his rating for server
             }
+            // update the change in specific client property//in my app:BL_imp.GetInstance().updateChild(c);
+            
         }
     }
 }
